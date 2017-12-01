1480e8bdf1363cfe7a13b23812b85561cf70755d
Max Winslow <maxtwinslow1@gmail.com>
2017-12-01 16:56:51 -0700

    Added new/create and edit/update actions for location controller

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,5 +1,5 @@
 class LocationsController < ApplicationController
-  before_action :location_set, only: [:show]
+  before_action :location_set, only: [:show, :edit, :update]
 
   def index
     @locations = Location.all
@@ -9,13 +9,35 @@ class LocationsController < ApplicationController
   end
 
   def new
+    @location = Location.new
+  end
+
+  def create
+    @location = Location.new(location_params)
+    if @location.save
+      redirect_to location_path(@location)
+    else
+      render 'new'
+    end
   end
 
   def edit
   end
 
+  def update
+    if @location.update(location_params)
+      redirect_to location_path(@location)
+    else
+      render 'edit'
+    end
+  end
+
   private
     def location_set
       @location = Location.find(params[:id])
+    end
+
+    def location_params
+      params.require(:location).permit(:name, :address)
     end
 end
