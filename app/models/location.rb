8d9cf50456dbaa291dcac403e332ad65778e677b
Max Winslow <maxtwinslow1@gmail.com>
2017-12-01 15:20:47 -0700

    Added locations controller

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,3 @@
 class Location < ApplicationRecord
   belongs_to :trip, optional: true
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'locations#index'
+  #will change root_path to trips#index later
+  #will add nested resources under trips later
+  resources :songs
 end
