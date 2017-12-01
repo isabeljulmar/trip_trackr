90e64ad728ec064750921bbb35fcde2705261fdb
Max Winslow <maxtwinslow1@gmail.com>
2017-12-01 15:54:17 -0700

    Added seed data for locations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,7 @@
+20.times do
 
+Location.create(
+            name: Faker::LordOfTheRings.location,
+            address: Faker::Address.street_address + " " + Faker::Address.city + " " + Faker::Address.state_abbr
+          )
+end
