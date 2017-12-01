0134376b688544b516fa28fcde8b6b03428cc87d
Max Winslow <maxtwinslow1@gmail.com>
2017-12-01 15:32:23 -0700

    Amended routes.rb to add nested routes locations within trips

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  root 'locations#index'
-  #will change root_path to trips#index later
-  #will add nested resources under trips later
-  resources :songs
+  resources :trips do
+    resources :locations
+  end
 end
