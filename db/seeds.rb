53a63bd7053aba7324e20fe1889f6847036f2345
Max Winslow <maxtwinslow1@gmail.com>
2017-12-01 18:00:30 -0700

    Finished seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,13 @@
 5.times do
-
-Trip.create(
-  trip_name: Faker::StarWars.character,
-  trip_duration: rand(1..31)
-)
+  Trip.create(
+    trip_name: Faker::StarWars.character,
+    trip_duration: rand(1..31)
+  )
 end
 
-
 20.times do
-
-Location.create(
+  Location.create(
             name: Faker::StarWars.planet,
-            address: rand(1000..9999) + Faker::StarWars.droid + " " + Faker::Address.city + " " + Faker::Address.state_abbr
+            address: rand(1000..9999).to_s + Faker::StarWars.droid + " " + Faker::Space.nebula
           )
 end
