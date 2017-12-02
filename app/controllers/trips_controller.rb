1027e51104e8cff627bce490844ecbbbed260dda
Max Winslow <maxtwinslow1@gmail.com>
2017-12-01 19:11:17 -0700

    refactored trips controller, rearranged edit and show page for trips, changed trip name to Star Wars species in Seed

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -15,13 +15,25 @@ before_action :set_trip, only: [:show, :edit, :update, :destroy]
   def edit
   end
 
+  def update
+    if @trip.update(trip_params)
+      flash[:success] = Faker::StarWars.quote
+      redirect_to trip_path(@trip)
+    else
+      render 'edit'
+    end
+  end
+
   def destroy
+    @trip.destroy
+    flash[:danger] = "Strike me down, and I will become more powerful than you could possibly imagine."
+    redirect_to trips_path
   end
 
 
   private
     def trip_params
-      params.require(:trip).permit(:name, :id)
+      params.require(:trip).permit(:trip_name, :trip_duration)
     end
 
     def set_trip
