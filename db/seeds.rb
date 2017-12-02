2252d25aca748f8ffbf0a72dc4f787c6e52d8d6c
Max Winslow <maxtwinslow1@gmail.com>
2017-12-01 19:11:17 -0700

    refactored trips controller, rearranged edit and show page for trips, changed trip name to Star Wars species in Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
-5.times do
+7.times do
   Trip.create(
-    trip_name: Faker::StarWars.character,
+    trip_name: Faker::StarWars.specie,
     trip_duration: rand(1..31)
   )
 end
 
-20.times do
+25.times do
   Location.create(
             name: Faker::StarWars.planet,
             address: rand(1000..9999).to_s + Faker::StarWars.droid + " " + Faker::Space.nebula
