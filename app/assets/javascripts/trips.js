fae9e9ac10f768370c0d811cf5170f7b91d9433e
Max Winslow <maxtwinslow1@gmail.com>
2017-12-03 22:34:33 -0700

    Added a silly animation and some additional front page styling

--- app/assets/javascripts/trips.js
+++ app/assets/javascripts/trips.js
@@ -1,2 +1,10 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+//Adds animation class to trips list on yoga img click
+$(document).ready(function(){
+  $('#yoda').click(function(){
+    $('body').addClass('animate-class')
+    $('#resize').css('animation', 'crawl 35s linear')
+  })
+})
